c4f35f810e180e7f37a54a2f219fc0d8c186a29e
ACEXRA <rasheed.786ahamed@gmail.com>
2023-09-15 08:10:36 +0530

    first commit

---
+++ frontend/src/store.jsx
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slice/AuthSlice";
+import { apiSlice } from "./slice/ApiSlice";
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
+
+export default store;
